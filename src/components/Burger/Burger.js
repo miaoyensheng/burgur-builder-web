215a9737aead582ef9e5e898da835203906984c1
miaoyensheng <miaoyensheng@gmail.com>
2019-08-27 19:32:22 +1000

    Created Burger and Layout component, and BurgerBuilder container
    Burger images are built using basic css manipulation and transformation in the Burger.module.css.
    A higher order component that does nothing other than wrapping elements.
    A basic skeleton outline of the layout is also added.

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+
+const burger = (props) => {
+
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey+i} type={igKey} />
+            });
+        });
+
+    // console.log(transformedIngredients);
+
+    return (
+        // <div>
+        [
+            <div key="lala">lalala</div>,
+            <div key="lolo">lololol</div>,
+            <div key="lulu">lulululul</div>
+        ]
+        // </div>
+
+
+
+        // <div className={classes.Burger}>
+        //     <BurgerIngredient type="bread-top" />
+        //     {transformedIngredients}
+        //     <BurgerIngredient type="bread-bottom" />
+        // </div>
+    );
+}
+
+export default burger;
